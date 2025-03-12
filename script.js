4f8e2bc63b61a195db995a15c7155f9328e56c8a
Sebastian Idesis <68556202+SebastianIdesis@users.noreply.github.com>
2025-03-12 16:32:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+// Define study
+const study = lab.util.fromObject({
+  "title": "root",
+  "type": "lab.flow.Sequence",
+  "parameters": {},
+  "plugins": [
+    {
+      "type": "lab.plugins.Metadata",
+      "path": undefined
+    },
+    {
+      "type": "lab.plugins.Download",
+      "filePrefix": "study",
+      "path": undefined
+    }
+  ],
+  "metadata": {
+    "title": "",
+    "description": "",
+    "repository": "",
+    "contributors": ""
+  },
+  "files": {},
+  "responses": {},
+  "content": [
+    {
+      "type": "lab.canvas.Screen",
+      "content": [
+        {
+          "type": "i-text",
+          "left": 0,
+          "top": 0,
+          "angle": 0,
+          "width": 329.09,
+          "height": 78.11,
+          "stroke": null,
+          "strokeWidth": 1,
+          "fill": "black",
+          "text": "Si funciona esto la vida\nva a ser mas facil",
+          "fontStyle": "normal",
+          "fontWeight": "normal",
+          "fontSize": 32,
+          "fontFamily": "sans-serif",
+          "lineHeight": 1.16,
+          "textAlign": "center"
+        }
+      ],
+      "viewport": [
+        800,
+        600
+      ],
+      "files": {},
+      "responses": {
+        "click": ""
+      },
+      "parameters": {},
+      "messageHandlers": {},
+      "title": "Screen"
+    },
+    {
+      "type": "lab.canvas.Screen",
+      "content": [
+        {
+          "type": "i-text",
+          "left": 11,
+          "top": -25,
+          "angle": 0,
+          "width": 52.2,
+          "height": 36.16,
+          "stroke": null,
+          "strokeWidth": 1,
+          "fill": "black",
+          "text": "Yes",
+          "fontStyle": "normal",
+          "fontWeight": "normal",
+          "fontSize": 32,
+          "fontFamily": "sans-serif",
+          "lineHeight": 1.16,
+          "textAlign": "center"
+        }
+      ],
+      "viewport": [
+        800,
+        600
+      ],
+      "files": {},
+      "responses": {
+        "click": ""
+      },
+      "parameters": {},
+      "messageHandlers": {},
+      "title": "Stimulus"
+    },
+    {
+      "type": "lab.canvas.Screen",
+      "content": [
+        {
+          "type": "i-text",
+          "left": 0,
+          "top": 0,
+          "angle": 0,
+          "width": 149.42,
+          "height": 36.16,
+          "stroke": null,
+          "strokeWidth": 1,
+          "fill": "black",
+          "text": "Thank you",
+          "fontStyle": "normal",
+          "fontWeight": "normal",
+          "fontSize": 32,
+          "fontFamily": "sans-serif",
+          "lineHeight": 1.16,
+          "textAlign": "center"
+        }
+      ],
+      "viewport": [
+        800,
+        600
+      ],
+      "files": {},
+      "responses": {
+        "click": ""
+      },
+      "parameters": {},
+      "messageHandlers": {},
+      "title": "Screen"
+    }
+  ]
+})
+
+// Let's go!
+study.run()
